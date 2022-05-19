8c35634d311b2c740abfc7293e68220d3d6b2ca9
Lex <greavis9@gmail.com>
2022-05-19 15:21:33 +0800

    tiny edt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return <h2>Hello!</h2>
+}
+
+const Field = () => {
+  const styleField = {
+    width: '300px'
+  };
+  return <input 
+          placeholder='Type here' 
+          type='text'
+          style={styleField}/>
+}
+
+function Btn() {
+  const text = 'Sign in';
+  const logged = false;
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
