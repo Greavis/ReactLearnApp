5a68e0f1e1173b2d653d332d0b2721cf1ff883a0
Lex <greavis9@gmail.com>
2022-05-19 15:43:55 +0800

    last

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import logo from './logo.svg';
+import {Component, StrictMode} from 'react';
 import './App.css';
 
 const Header = () => {
-  return <h2>Hello!</h2>
+    return <h2>Hello!</h2>
 }
 
-const Field = () => {
-  const styleField = {
-    width: '300px'
-  };
-  return <input 
-          placeholder='Type here' 
-          type='text'
-          style={styleField}/>
+// const Field = () => {
+//     const styleField = {
+//       width: '300px'
+//     };
+//     return <input 
+//             placeholder='Type here' 
+//             type='text'
+//             style={styleField}/>
+// }
+
+class Field extends Component {
+    render() {
+        const inputField = 
+        <div>
+            <input type='text'/>
+        </div>
+        return inputField;
+    }
 }
 
 function Btn() {
-  const text = 'Sign in';
-  const logged = false;
-  return <button>{logged ? 'Enter' : text}</button>
+    const text = 'Sign in';
+    const logged = false;
+    return <button>{logged ? 'Enter' : text}</button>
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Field/>
-      <Btn/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <StrictMode>
+                <Header/>
+            </StrictMode>
+            <Field/>
+            <Btn/>
+        </div>
+    );
 }
 
 export {Header};
